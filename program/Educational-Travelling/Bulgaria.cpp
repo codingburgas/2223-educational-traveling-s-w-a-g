f451738cc608c6e0ffc23d41a7520da3b47a98cc
vvbolashikova20 <vvbolashikova20@codingburgas.bg>
2022-11-20 02:17:05 +0200

    Bug fixes

--- program/Educational-Travelling/Bulgaria.cpp
+++ program/Educational-Travelling/Bulgaria.cpp
@@ -11,8 +11,25 @@ int bulgariaQuest()
         ClearBackground(BLACK);
         DrawTextEx(font, "Coming soon...", 
             { (SCREEN_WIDTH - MeasureTextEx(font, "Coming soon...", 30, 5).x) / 2, 
-            (SCREEN_HEIGHT - MeasureTextEx(font, "Coming soon...", 30, 5).y) / 2 },
+            200 },
             30, 5, WHITE);
+
+        DrawRectangleLinesEx({ (SCREEN_WIDTH - 250) / 2, 350, 250, 75 }, 6, WHITE);
+        DrawTextEx(font, "Back to map",
+            { (250 - MeasureTextEx(font, "Back to map", 25, 5).x) / 2 + (SCREEN_WIDTH - 250) / 2,
+              (75 - MeasureTextEx(font, "Back to map", 25, 5).y) / 2 + 350 },
+            25, 5, WHITE);
+
+        if (CheckCollisionPointRec(GetMousePosition(), { (SCREEN_WIDTH - 250) / 2, 350, 250, 75 }))
+        {
+            DrawRectangleRec({ (SCREEN_WIDTH - 250) / 2, 350, 250, 75 }, WHITE);
+            DrawTextEx(font, "Back to map",
+                { (250 - MeasureTextEx(font, "Back to map", 25, 5).x) / 2 + (SCREEN_WIDTH - 250) / 2,
+                  (75 - MeasureTextEx(font, "Back to map", 25, 5).y) / 2 + 350 },
+                25, 5, BLACK);
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+                drawMap();
+        }
 
         EndDrawing();
     }
